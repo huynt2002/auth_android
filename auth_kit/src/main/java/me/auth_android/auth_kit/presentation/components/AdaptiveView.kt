a9b318a9b9ddf5131a4b97dabeb8960e8259c2b3
huynt2002 <uchitoyuki@gmail.com>
2025-03-23 17:52:33 +0700

    refactor

--- auth_kit/src/main/java/me/auth_android/auth_kit/presentation/components/AdaptiveView.kt
+++ auth_kit/src/main/java/me/auth_android/auth_kit/presentation/components/AdaptiveView.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,17 +28,31 @@ fun AdaptiveView(content: @Composable () -> Unit) {
 
 @Composable
 private fun CompactView(content: @Composable () -> Unit) {
-    Box(modifier = Modifier.fillMaxSize()) { content() }
+    Scaffold { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            content()
+        }
+    }
+
 }
 
 @Composable
 private fun MediumOrExpandView(content: @Composable () -> Unit) {
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.5f)),
         contentAlignment = Alignment.Center,
     ) {
         Box(
-            modifier = Modifier.height(600.dp).width(450.dp).clip(shape = RoundedCornerShape(16.dp))
+            modifier = Modifier
+                .height(600.dp)
+                .width(450.dp)
+                .clip(shape = RoundedCornerShape(16.dp))
         ) {
             content()
         }
